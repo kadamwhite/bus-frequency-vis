03085ae8d30fee8edb8e7029636e01d7e1a1d766
K. Adam White <kadamwhite@users.noreply.github.com>
2015-06-13 23:00:00 -0400

    Despense with the fs nonsense, just require the json

--- routes/data.js
+++ routes/data.js
@@ -3,17 +3,10 @@
 var express = require( 'express' );
 var router = express.Router();
 
-var fs = require( 'fs' );
-var path = require( 'path' );
+var data = require( '../data.json' );
 
 router.get( '/', function( req, res, next ) {
-  var fileName = path.join( __dirname, '../data.json' );
-  fs.readFile( fileName, function( err, data ) {
-    res.status( 200 ).json( JSON.parse( data ) );
-  }).catch(function( err ) {
-    console.error( err );
-    res.status( 500 ).json( err.message );
-  });
+  res.status( 200 ).json( data );
 });
 
 module.exports = router;
